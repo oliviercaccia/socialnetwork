785e1a31fc83429b7ab0099eac9d4ed90fc82b68
oliviercaccia <olivier.caccia.pro@gmail.com>
2021-04-26 17:00:54 +0200

    Remove the hardcoded comments

--- controllers/controller.php
+++ controllers/controller.php
@@ -1,6 +1,11 @@
 <?php
 
-$action = $_GET["action"] ?? "display";
+//$action = $_GET["action"] ?? "display";
+/*$action = "display";
+if (isset($_GET["action"])) {
+  $action = $_GET["action"];
+}*/
+$action = isset($_GET["action"]) ? $_GET["action"] : "display";
 
 switch ($action) {
 
@@ -32,16 +37,19 @@ switch ($action) {
     include "../models/CommentManager.php";
     $comments = array();
 
+    foreach ($posts as $onePosts) {
+      $comments[$onePosts['id']] =  GetAllCommentsFromPostId($onePosts['id']);
+    };
     // ===================HARDCODED PART===========================
     // format idPost => array of comments
-    $comments[1] = array(
-      array("nickname" => "FakeUser1", "created_at" => "1970-01-01 00:00:00", "content" => "Fake comment 01."),
-      array("nickname" => "FakeUser2", "created_at" => "1970-01-02 00:00:00", "content" => "Fake comment 02."),
-      array("nickname" => "FakeUser1", "created_at" => "1970-01-03 00:00:00", "content" => "Fake comment 03.")
-    );
-    $comments[3] = array(
-      array("nickname" => "FakeUser1", "created_at" => "1970-01-01 00:00:00", "content" => "Fake comment 04."),
-    );
+    // $comments[1] = array(
+    //   array("nickname" => "FakeUser1", "created_at" => "1970-01-01 00:00:00", "content" => "Fake comment 01."),
+    //   array("nickname" => "FakeUser2", "created_at" => "1970-01-02 00:00:00", "content" => "Fake comment 02."),
+    //   array("nickname" => "FakeUser1", "created_at" => "1970-01-03 00:00:00", "content" => "Fake comment 03.")
+    // );
+    // $comments[3] = array(
+    //   array("nickname" => "FakeUser1", "created_at" => "1970-01-01 00:00:00", "content" => "Fake comment 04."),
+    // );
     // =============================================================
 
     include "../views/DisplayPosts.php";
